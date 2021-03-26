ae98251afbe9a20100d475e2b0f67f1448671ace
rbeard0330 <@dul2k3BKW6m>
2021-03-26 16:13:03 -0400

    Add comment to suppress eslint in generated TS output

--- pydantic2ts/cli/script.py
+++ pydantic2ts/cli/script.py
@@ -199,6 +199,7 @@ def generate_typescript_defs(
     banner_comment = "\n".join(
         [
             "/* tslint:disable */",
+            "/* eslint-disable */",
             "/**",
             "/* This file was automatically generated from pydantic models by running pydantic2ts.",
             "/* Do not modify it by hand - just update the pydantic models and then re-run the script",
